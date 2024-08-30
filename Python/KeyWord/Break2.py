aee3f3d7d00ae4a511ffe38c472c407b602696a4
GraceGajmer <nelsontv35@gmail.com>
2024-08-30 17:59:31 +0545

    public

---
+++ Python/KeyWord/Break2.py
@@ -0,0 +1,9 @@
+a=input("Enter a Word")
+
+for i in a:
+    if (a=='A'):
+     print("A is Found")
+     break
+    else:
+     print("A is not found    ")
+
---
+++ Python/KeyWord/Return1.py
@@ -0,0 +1,24 @@
+def sum(a,b):
+    return(a+b)
+
+sum = sum(5,5)
+
+def sub(c,d):
+    return(c-d)
+
+sub = sub(5,5)
+
+def mul(e,f):
+    return(e*f)
+
+mul = mul(5,5)
+
+def div(g,h):
+    return(g/h)
+
+div = div(5,5)
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
